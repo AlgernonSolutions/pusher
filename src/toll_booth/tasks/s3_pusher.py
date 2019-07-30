0630c568ad750d50a454e2949a0e6d556cbf5686
Jeff <jcubeta@algernon.solutions>
2019-07-30 14:44:29 -0400

    added standard responses for operations

--- src/toll_booth/tasks/s3_pusher.py
+++ src/toll_booth/tasks/s3_pusher.py
@@ -1,4 +1,7 @@
+import json
 import os
+from datetime import datetime
+from decimal import Decimal
 from typing import Union
 
 import boto3
@@ -6,6 +9,16 @@ import rapidjson
 from botocore.exceptions import ClientError
 
 from toll_booth.obj.scalars.inputs import InputVertex, InputEdge
+
+
+class FireHoseEncoder(json.JSONEncoder):
+    @classmethod
+    def default(cls, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super(FireHoseEncoder, cls()).default(obj)
 
 
 def _check_for_object(s3_object):
@@ -23,9 +36,25 @@ def _store_to_s3(bucket_name, base_file_key, scalar: Union[InputVertex, InputEdg
     s3_resource = boto3.resource('s3')
     s3_object = s3_resource.Object(bucket_name, file_key)
     if _check_for_object(s3_object):
-        return f'object at {file_key} already exists in {bucket_name}'
-    s3_object.put(Body=rapidjson.dumps(scalar.for_index))
-    return f'stored successfully to {bucket_name} at {file_key}'
+        return {
+            'status': 'failed',
+            'operation': 'store_to_s3',
+            'details': {
+                'message': f'object at {file_key} already exists in {bucket_name}',
+                'bucket_name': bucket_name,
+                'file_key': file_key
+            }
+        }
+    s3_object.put(Body=rapidjson.dumps(scalar.for_index, default=FireHoseEncoder.default))
+    return {
+            'status': 'succeeded',
+            'operation': 'store_to_s3',
+            'details': {
+                'message': '',
+                'bucket_name': bucket_name,
+                'file_key': file_key
+            }
+        }
 
 
 def s3_handler(source_vertex: InputVertex, edge: InputEdge = None, target_vertex: InputVertex = None, **kwargs):
