a317b494853e52b0e48bf338f75680dbfb155bd3
Jeff <jcubeta@algernon.solutions>
2019-07-30 14:12:52 -0400

    corrected missing config value

--- src/toll_booth/tasks/graph_pusher.py
+++ src/toll_booth/tasks/graph_pusher.py
@@ -1,9 +1,13 @@
+import logging
+
 from toll_booth.obj.graph.ogm import Ogm
 
 
 def graph_handler(source_vertex, **kwargs):
+    logging.info(f'received a call to the graph_handler: {source_vertex}, {kwargs}')
     graph_results = {}
     ogm = Ogm()
+    logging.info(f'created ogm: {ogm}')
     edge = kwargs.get('edge')
     target_vertex = kwargs.get('target_vertex')
     graph_results['source_vertex'] = ogm.graph_vertex(source_vertex)
--- tests/integration/test_pusher.py
+++ tests/integration/test_pusher.py
@@ -5,12 +5,14 @@ import toll_booth
 
 @pytest.mark.pusher_i
 class TestPusher:
+    def test_graph_pusher(self, push_event, mock_context):
+        push_event['push_type'] = 'graph'
+        results = toll_booth.handler(push_event, mock_context)
+        assert results
+
     def test_index_pusher(self, push_event, mock_context):
         push_event['push_type'] = 'index'
         results = toll_booth.handler(push_event, mock_context)
         assert results
 
-    def test_graph_pusher(self, push_event, mock_context):
-        push_event['push_type'] = 'graph'
-        results = toll_booth.handler(push_event, mock_context)
-        assert results
+
