9c1fffe123f0bcfdafa55dfff7df0eb23aca9955
Jeff <jcubeta@algernon.solutions>
2019-07-30 11:42:54 -0400

    added parallel processing to handler

--- src/toll_booth/handler.py
+++ src/toll_booth/handler.py
@@ -1,19 +1,49 @@
 import logging
+from collections import deque
+from queue import Queue
+from threading import Thread
 
 from algernon.aws import lambda_logged
+from algernon import ajson
 from toll_booth import tasks
+
+
+def _run_handler(work_queue, results):
+    while True:
+        task = work_queue.get()
+        if task is None:
+            return
+        leech_result = task['leech_result']
+        push_type = task['push_type']
+        push_kwargs = task.get('push_kwargs', {})
+        source_vertex = leech_result['source_vertex']
+        edge = leech_result.get('edge')
+        target_vertex = leech_result.get('target_vertex')
+        pusher = getattr(tasks, f'{push_type}_handler', None)
+        if pusher is None:
+            raise RuntimeError(f'do not know how to push object for {push_type}')
+        push_results = pusher(source_vertex, edge, target_vertex, **push_kwargs)
+        results.append(push_results)
 
 
 @lambda_logged
 def handler(event, context):
     logging.info(f'received a call to push an object to persistence: {event}/{context}')
+    work_queue = Queue()
+    results = deque()
     push_type = event['push_type']
     leech_results = event['aio']
-    source_vertex = leech_results['source_vertex']
-    edge = leech_results.get('edge')
-    target_vertex = leech_results.get('target_vertex')
     push_kwargs = event.get('push_kwargs', {})
-    pusher = getattr(tasks, f'{push_type}_handler', None)
-    if pusher is None:
-        raise RuntimeError(f'do not know how to push object for {push_type}')
-    push_results = pusher(source_vertex, edge, target_vertex, **push_kwargs)
+    workers = []
+    num_workers = event.get('num_workers', 5)
+    for _ in range(num_workers):
+        worker = Thread(target=_run_handler, args=(work_queue, results))
+        worker.start()
+        workers.append(worker)
+    for entry in leech_results:
+        work_queue.put({'leech_result': entry, 'push_type': push_type, 'push_kwargs': push_kwargs})
+    for _ in workers:
+        work_queue.put(None)
+    for worker in workers:
+        worker.join()
+    return ajson.dumps([x for x in results])
